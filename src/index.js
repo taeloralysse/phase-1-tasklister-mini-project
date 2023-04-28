cfa2be3a0d19f458f81e79c073080fa851780705
Taelor Lambert <taeloralysse@gmail.com>
2023-04-28 14:52:54 -0400

    idk

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  const form = document.querySelector("#create-task-form")
+  form.addEventListener("submit", function (event) {
+    event.preventDefault()
+    console.log(event.target.querySelector("#new-task-description").value)
+    form.reset()
+  })
+  })
+
